654c72dbd7503bb4aaa857735934398bcc61d6f0
Sam Lalli <2sam.lalli@gmail.com>
2020-09-11 10:09:32 -0600

    API fetched, rendered on page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+
+
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [player, SetPlayer] = useState([])
 
+  useEffect(() => {
+
+    axios.get("http://swapi.dev/api/people")
+      .then(res => {
+        SetPlayer(res.data.results)
+        //console.log(res.data.results)   
+      })
+      .catch(err => {console.log("Failed to Fetch")})
+  },[])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div>
+        {
+          player.map(pl =>{
+            return <Character key={pl.id} player={pl} />
+          })
+
+        }
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
-// Write your Character component here
+import React, { useState } from 'react'
+
+
+const Character = props =>{
+    const { player } = props
+    const [toggleDetails, SetToggleDetails] = useState(false)
+
+    const Details = () => {
+        SetToggleDetails(!toggleDetails)
+        console.log(toggleDetails)
+    }
+
+    return(
+        <>
+        <div>
+        <h1>{player.name}</h1>
+        <button onClick ={() => {Details()}}>Details</button>
+        {toggleDetails
+        ? <p>Born: {player.birth_year}<br></br>Gender: {player.gender}<br></br>Height: {player.height}</p>
+        : null }
+        </div>
+
+        </>
+    )
+}
+
+export default Character
